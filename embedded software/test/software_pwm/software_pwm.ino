68bba9b6e1e4b83762b5a3383ce178fbe3b95b34
Abdullah Naeem <64824714+abdullahworld@users.noreply.github.com>
2023-04-09 13:27:36 +1200

    software pwm tested

---
+++ embedded software/test/software_pwm/software_pwm.ino
@@ -0,0 +1,63 @@
+#include <stdint.h>
+
+
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+//User controls
+#define R_POT A1
+#define AUTO 8 //S3
+#define BUT_CCW 7 //S2
+#define BUT_CW 5 //S1
+
+//Motor controls
+#define INPUT_A 3
+#define INPUT_B 4
+#define P1_INT digitalPinToInterrupt(INPUT_A)
+#define SLEEP 10
+#define MODE 6
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+int r_pot;
+
+struct typedef
+{
+  uint16_t period;
+  uint16_t elapsed_time;
+  
+}
+
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  lcd.init();
+
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("System starting");
+  delay(1000);
+  lcd.clear();
+
+  //configuring h-bridge driver circutry.  
+  pinMode(INPUT_A, OUTPUT);
+  pinMode(INPUT_B, OUTPUT);
+}
+
+void software_pwm()
+{
+  digitalWrite(INPUT_A, HIGH);
+  digitalWrite(INPUT_B, LOW);
+  delayMicroseconds(5000);
+  digitalWrite(INPUT_A, LOW);
+  digitalWrite(INPUT_B, HIGH);
+  delayMicroseconds(5000);
+}
+ 
+
+void loop() 
+{
+  software_pwm();
+}
