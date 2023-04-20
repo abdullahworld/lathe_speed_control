639b9ccb4740f3b27c4c02ddaff16c247f17a948
Abdullah Naeem <64824714+abdullahworld@users.noreply.github.com>
2023-04-20 23:06:45 +1200

    choose cheat way to start the hardware pwm

--- embedded software/test/hardware_pwm/hardware_pwm.ino
+++ embedded software/test/hardware_pwm/hardware_pwm.ino
@@ -3,4 +3,19 @@
  * https://docs.arduino.cc/tutorials/generic/secrets-of-arduino-pwm
  * Read above to understand how to create a hardware pwm then use it to test reference 
  * speed code.
-* /
+**/
+
+#define EN 3
+
+void setup()
+{
+    Serial.begin(9600);
+
+    pinMode(EN, OUTPUT);
+}
+
+void loop()
+{
+    analogWrite(EN, 125);
+    Serial.println("Hi");
+}
